3a66e1d2c58a97727877bd43bc2781815d76ef73
ciscuscus <67182842+ciscuscus@users.noreply.github.com>
2021-07-14 23:53:12 +0700

    Delete script.js

--- bookshelf-apps/js/script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function(){
-    const formInput = document.getElementById("inputBook");
-
-    formInput.addEventListener("submit", function(event) {
-        event.preventDefault();
-        addBook();
-    });
-})
