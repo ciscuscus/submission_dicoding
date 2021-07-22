520e49b75f85772f05ca8b8ce0f28c5fb7cd63b7
ciscuscus <67182842+ciscuscus@users.noreply.github.com>
2021-07-22 10:11:22 +0700

    Update dom.js

--- js/dom.js
+++ js/dom.js
@@ -1,6 +1,6 @@
 const INCOMPLETE_BOOK = "incompleteBookshelfList";
 
-function makeBook(inputBookTitle, inputBookAuthor, inputBookYear){
+function makeBook(inputBookTitle, inputBookAuthor, inputBookYear, inputBookIsComplete){
 
     const incompleteBookTitle = document.createElement("h2");
     incompleteBookTitle.innerText = inputBookTitle;
@@ -10,14 +10,18 @@ function makeBook(inputBookTitle, inputBookAuthor, inputBookYear){
 
     const incompleteBookYear = document.createElement("p");
     incompleteBookYear.innerText = inputBookYear;
+	
+	const incompleteBookIsComplete = document.createElement("p");
+    incompleteBookIsComplete.innerText = inputBookIsComplete;
 
     const textContainer = document.createElement("div");
     textContainer.classList.add("inner");
-    textContainer.append(incompleteBookTitle, incompleteBookAuthor, incompleteBookYear);
+    textContainer.append(incompleteBookTitle, incompleteBookAuthor, incompleteBookYear, incompleteBookIsComplete);
 
     const container = document.createElement("div");
     container.classList.add("item", "shadow");
     return container.appendChild(textContainer);
+	
 }
 
 function addBook(){
@@ -25,9 +29,9 @@ function addBook(){
     const inputBookTitle = document.getElementById("inputBookTitle").value;
     const inputBookAuthor = document.getElementById("inputBookAuthor").value;
     const inputBookYear = document.getElementById("inputBookYear").value;
-    //const inputBookIsComplete = document.getElementById("inputBookIsComplete").checked;
+    const inputBookIsComplete = document.getElementById("inputBookIsComplete").checked;
 
-        const tes = makeBook(inputBookTitle, inputBookAuthor, inputBookYear);
+        const tes = makeBook(inputBookTitle, inputBookAuthor, inputBookYear, inputBookIsComplete);
         incompleteBook.append(tes);
   
     
